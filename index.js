b5c59584b62119889247244aa22ccd0aa02afa30
Hanyeol Cho <hanyeol.cho@gmail.com>
2021-05-15 18:23:56 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,116 @@
+var module = (function() {
+    const webjs = require("webjs-helper");
+
+    var _id = "", _dir_path = "", _handlers = [];
+    var _web_prefix = "", _sockets = {};
+    var _web_loaded = false;
+
+    function _create_socket(sockid) {
+        return (function() {
+            var _handlers = {};
+
+            return {
+                send: function(data) {
+                    return new Promise(function(resolve, reject) {
+                        webjs.call("sendData", [ sockid, data ])
+                            .then(function() {
+                                resolve();
+                            })
+                            .catch(function(error) {
+                                reject(error);
+                            });
+                    });
+                },
+
+                on: function(event, handler) {
+                    _handlers[event] = handler;
+                },
+
+                _handle_event: function(event, data) {
+                    if (event in _handlers) {
+                        _handlers[event](data);
+                    }
+                }
+            }
+        })();
+    }
+
+    function _on_web_loaded(data) {
+        if (data["url"].startsWith("http://localhost")) {
+            webjs.import(_dir_path + "/websocket.js");
+
+            _handlers.forEach(function(handler) {
+                handler();
+            });
+
+            _web_loaded = true, _handlers = [];
+        }
+    }
+
+    function _on_socket_event(data) {
+        var sockid = data["socket"] || 0;
+
+        if (sockid in _sockets) {
+            _sockets[sockid]._handle_event(data["event"], data["data"]);
+        }
+    }
+
+    return {
+        initialize: function(id) {
+            var web_prefix = id.replace(".", "_");
+            var dir_path = this.__ENV__["dir-path"];
+
+            global[web_prefix + "__on_web_loaded"] = function(data) {
+                _on_web_loaded(data);
+            }
+            global[web_prefix + "__on_socket_event"] = function(data) {
+                _on_socket_event(data);
+            }
+
+            webjs.initialize(id + ".web", "__$_bridge");
+            view.object(id).action("load", { 
+                "filename": dir_path + "/web.sbml",
+                "dir-path": dir_path,
+                "web-id": id, 
+                "web-prefix": web_prefix
+            });
+
+            _id = id, _dir_path = dir_path;
+            _web_prefix = web_prefix;
+
+            return this;
+        },
+        
+        create: function(url, protocols) {
+            return new Promise(function(resolve, reject) {
+                var handler = function() {
+                    webjs.call("createSocket", [ _web_prefix, url, protocols ])
+                        .then(function(sockid) {
+                            var socket = _create_socket(sockid);
+
+                            _sockets[sockid] = socket;
+
+                            resolve(socket);
+                        })
+                        .catch(function(error) {
+                            reject(error);
+                        });
+                }
+
+                _web_loaded ? handler() : _handlers.push(handler);
+            });
+        },
+
+        destroy: function(socket) {
+            return new Promise(function(resolve, reject) {
+                var handler = function() {
+                    
+                }
+
+                _web_loaded ? handler() : _handlers.push(handler);
+            });
+        },
+    }
+})();
+
+__MODULE__ = module;
---
+++ websocket.js
@@ -0,0 +1,69 @@
+const _socketMap = {}
+
+function createSocket(owner, url, protocols, onResult, onError) {
+    var socket = new WebSocket(url, protocols);
+    var sockid = (Math.random() * 10000).toFixed(0);
+
+    socket.onopen = function(event) {
+        if (sockid in _socketMap) {
+            _notifyEvent(owner, "open", sockid);
+        }
+    }
+
+    socket.onmessage = function(event) {
+        if (sockid in _socketMap) {
+            _notifyEvent(owner, "message", sockid, event.data);
+        }
+    }
+    
+    socket.onclose = function(event) {
+        if (sockid in _socketMap) {
+            _notifyEvent(owner, "close", sockid);
+        }
+    }
+    
+    socket.onerror = function() {
+        if (sockid in _socketMap) {
+            _notifyEvent(owner, "error", sockid);
+        }
+    }
+    
+    _socketMap[sockid] = socket;
+
+    onResult(sockid);
+}
+
+function destroySocket(sockid, onResult, onError) {
+    if (sockid in _socketMap) {
+        var socket = _socketMap[sockid];
+
+        socket.terminate();
+
+        delete _socketMap[sockid];
+
+        onResult();
+    } else {
+        onError({ code: 404 });
+    }
+}
+
+function sendData(sockid, send) {
+    if (sockid in _socketMap) {
+        var socket = _socketMap[sockid];
+
+        socket.send(send);
+
+        onResult();
+    } else {
+        onError({ code: 404 });
+    }
+}
+
+function _notifyEvent(owner, event, sockid, data) {
+    __$_bridge.postMessage(JSON.stringify({
+        "script": owner + "__on_socket_event",
+        "socket": sockid,
+        "event": event,
+        "data": JSON.stringify(data)
+    }));
+}
